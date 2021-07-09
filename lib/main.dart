38da830bf8a19f621b93606e29f65a1fb6df34f2
Dedy Setiadi <dedysetiadi1708@gmail.com>
2021-07-09 13:51:13 +0700

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,59 @@
-
-
 import 'package:flutter/material.dart';
-
-
-
+import 'package:splashscreen/splashscreen.dart';
+import 'package:splashscreen/splashscreen.dart';
 import './screens/category_screen.dart';
 import 'places/places_screen.dart';
-import 'screens/detail_screen.dart'; //import file categoryscreen yang nantinya berfungsi untuk menampilkan list kategori
+import 'screens/detail_screen.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new MyApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget { 
-
-
-
- 
+class MyApp extends StatefulWidget {
   @override
+  _MyAppState createState() => new _MyAppState();
+}
 
-  
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 8,
+      backgroundColor: Colors.black,
+      image: Image.asset('assets/loading.gif'),
+      loaderColor: Colors.white,
+      photoSize: 150.0,
+      navigateAfterSeconds: MainScreen(),
+    );
+  }
+}
 
-    
-    var copyWith = ThemeData.light().textTheme.copyWith(
-          // ignore: deprecated_member_use
-          title: TextStyle(fontWeight: FontWeight.bold, fontSize: 20), //TITLE KITA GUNAKAN BOLD DAN SIZE 20
-          // ignore: deprecated_member_use
-          subhead: TextStyle(fontWeight: FontWeight.bold), //SUBHEADNYA CUKUP BOLD SAJA
-        );
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aplikasi Berita',
+      title: 'Aplikasi Wisata',
       //KITA SET THEME-NYA SECARA UMUM AGAR KETIKA MELAKUKAN PERUBAHAN, CUKUP PADA SETTING DIBAWAH AKAN MEMBERIKAN EFEK KE SEMUA PAGE YANG ADA
       theme: ThemeData(
-        primaryColor: Colors.pinkAccent, //WARNA UTAMA KITA GUNAKAN pinkAccent
-        accentColor: Colors.pink, //SECONDARY COLORNYA KITA SET KE PINK
-        canvasColor: Color.fromRGBO(255, 254, 229, 1), //WARNA BACKGROUND CANVASNYA PAKAI RGBO DIMANA CODE DIATAS AKAN MENGHASILKAN WARNA KUNING 
-      
-        //SET JUGA KONFIGURASI UNTUK TEXT-NYA
-        textTheme: copyWith
+          primaryColor: Colors.pinkAccent, //WARNA UTAMA KITA GUNAKAN pinkAccent
+          accentColor: Colors.pink, //SECONDARY COLORNYA KITA SET KE PINK
+          canvasColor: Color.fromRGBO(255, 254, 229, 1), //WARNA BACKGROUND CANVASNYA PAKAI RGBO DIMANA CODE DIATAS AKAN MENGHASILKAN WARNA KUNING
+
       ),
-        
+
       //SET ROUTING YANG AKAN MENGATUR ALUR APLIKASI
       routes: {
         '/': (ctx) => CategoryScreen(),  //DIMANA ROOT ROUTINGNYA AKAN ME-LOAD CategoryScreen
         '/places': (ctx) => PlacesScreen(),
         '/detail': (ctx) => DetailScreen() //TAMBAHKAN LINE INI
-},// TANDA / BERARTI SECARA DEFAULT KETIKA APLIKASI DI-LOAD MAKA ROUTE TERSEBUT AKAN DIJALANKAN
-      
-  
+      },// TANDA / BERARTI SECARA DEFAULT KETIKA APLIKASI DI-LOAD MAKA ROUTE TERSEBUT AKAN DIJALANKAN
+
+
     );
   }
 }
+
+
